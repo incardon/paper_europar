8b164ed2d84dace0c22faeb0c925fefc3de74025
Aryaman Gupta <argupta@mpi-cbg.de>
2022-01-06 04:01:22 +0530

    updates kokkos benchmark

--- benchmarks/mem_bound/Kokkos/membound.cpp
+++ benchmarks/mem_bound/Kokkos/membound.cpp
@@ -1,142 +1,87 @@
-/*
-//@HEADER
-// ************************************************************************
-//
-//                        Kokkos v. 3.0
-//       Copyright (2020) National Technology & Engineering
-//               Solutions of Sandia, LLC (NTESS).
-//
-// Under the terms of Contract DE-NA0003525 with NTESS,
-// the U.S. Government retains certain rights in this software.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-// 1. Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright
-// notice, this list of conditions and the following disclaimer in the
-// documentation and/or other materials provided with the distribution.
-//
-// 3. Neither the name of the Corporation nor the names of the
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY NTESS "AS IS" AND ANY
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL NTESS OR THE
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// Questions? Contact Christian R. Trott (crtrott@sandia.gov)
-//
-// ************************************************************************
-//@HEADER
-*/
-
-//
-// First Kokkos::View (multidimensional array) example:
-//   1. Start up Kokkos
-//   2. Allocate a Kokkos::View
-//   3. Execute a parallel_for and a parallel_reduce over that View's data
-//   4. Shut down Kokkos
-//
-// Compare this example to 03_simple_view_lambda, which uses C++11
-// lambdas to define the loop bodies of the parallel_for and
-// parallel_reduce.
-//
-
 #include <Kokkos_Core.hpp>
 #include <cstdio>
 
-// A Kokkos::View is an array of zero or more dimensions.  The number
-// of dimensions is specified at compile time, as part of the type of
-// the View.  This array has two dimensions.  The first one
-// (represented by the asterisk) is a run-time dimension, and the
-// second (represented by [3]) is a compile-time dimension.  Thus,
-// this View type is an N x 3 array of type double, where N is
-// specified at run time in the View's constructor.
-//
-// The first dimension of the View is the dimension over which it is
-// efficient for Kokkos to parallelize.
 using view_type = Kokkos::View<double * [3]>;
 
-// parallel_for functor that fills the View given to its constructor.
-// The View must already have been allocated.
-struct InitView {
-  view_type a;
-
-  // Views have "view semantics."  This means that they behave like
-  // pointers, not like std::vector.  Their copy constructor and
-  // operator= only do shallow copies.  Thus, you can pass View
-  // objects around by "value"; they won't do a deep copy unless you
-  // explicitly ask for a deep copy.
-  InitView(view_type a_) : a(a_) {}
-
-  // Fill the View with some data.  The parallel_for loop will iterate
-  // over the View's first dimension N.
-  KOKKOS_INLINE_FUNCTION
-  void operator()(const int i) const {
-    // Acesss the View just like a Fortran array.  The layout depends
-    // on the View's memory space, so don't rely on the View's
-    // physical memory layout unless you know what you're doing.
-    a(i, 0) = 1.0 * i;
-    a(i, 1) = 1.0 * i * i;
-    a(i, 2) = 1.0 * i * i * i;
-  }
+struct ele
+{
+  double s;
+  double v[2];
+  double t[2][2];
 };
 
-// Reduction functor that reads the View given to its constructor.
-struct ReduceFunctor {
-  view_type a;
+void struct_of_arrays (Kokkos::View<double*> prp0, Kokkos::View<double*[2]> prp1, Kokkos::View<double*[2][2]> prp2, Kokkos::View<double*[2]> pos, int N) {
 
-  // Constructor takes View by "value"; this does a shallow copy.
-  ReduceFunctor(view_type a_) : a(a_) {}
+      Kokkos::parallel_for(
+        N, KOKKOS_LAMBDA(const int i) {
+          prp0(i) = pos(i, 0) + pos(i, 1);
+         
+          prp1(i, 0) = pos(i, 0);
+          prp1(i, 1) = pos(i, 1);
 
-  // If you write a functor to do a reduction, you must specify the
-  // type of the reduction result via a public 'value_type' alias.
-  using value_type = double;
+          prp2(i, 0, 0) = pos(i, 0);
+          prp2(i, 0, 1) = pos(i, 1);
+          prp2(i, 1, 0) = pos(i, 0) + pos(i, 1);
+          prp2(i, 1, 1) = pos(i, 1) - pos(i, 0);
 
-  KOKKOS_INLINE_FUNCTION
-  void operator()(int i, double& lsum) const {
-    lsum += a(i, 0) * a(i, 1) / (a(i, 2) + 0.1);
-  }
-};
+          pos(i, 0) += 0.01;
+          pos(i, 1) += 0.01;
+
+        });
+}
+
+void array_of_structs (Kokkos::View<ele*> prp, Kokkos::View<double*[2]> pos, int N) {
+
+        Kokkos::parallel_for(
+        N, KOKKOS_LAMBDA(const int i) {
+          prp(i).s = pos(i, 0) + pos(i, 1);
+         
+          prp(i).v[0] = pos(i, 0);
+          prp(i).v[1] = pos(i, 1);
+
+          prp(i).t[0][0] = pos(i, 0);
+          prp(i).t[0][1] = pos(i, 1);
+          prp(i).t[1][0] = pos(i, 0) + pos(i, 1);
+          prp(i).t[1][1] = pos(i, 1) - pos(i, 0);
+
+          pos(i, 0) += 0.01;
+          pos(i, 1) += 0.01;
+
+        });
+}
 
 int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
+
+  printf("Hello world\n");
+
   {
-    const int N = 10;
 
-    // Allocate the View.  The first dimension is a run-time parameter
-    // N.  We set N = 10 here.  The second dimension is a compile-time
-    // parameter, 3.  We don't specify it here because we already set it
-    // by declaring the type of the View.
-    //
-    // Views get initialized to zero by default.  This happens in
-    // parallel, using the View's memory space's default execution
-    // space.  Parallel initialization ensures first-touch allocation.
-    // There is a way to shut off default initialization.
-    //
-    // You may NOT allocate a View inside of a parallel_{for, reduce,
-    // scan}.  Treat View allocation as a "thread collective."
-    //
-    // The string "A" is just the label; it only matters for debugging.
-    // Different Views may have the same label.
-    view_type a("A", N);
+    int N = 16777216;
+    Kokkos::View<double*[2]> pos ("position", N);
+    Kokkos::View<double*> prp_s ("scalar_property", N);
+    Kokkos::View<double*[2]> prp_v ("vector_property", N);
+    Kokkos::View<double*[2][2]> prp_t ("tensor_property", N);
 
-    Kokkos::parallel_for(N, InitView(a));
-    double sum = 0;
-    Kokkos::parallel_reduce(N, ReduceFunctor(a), sum);
-    printf("Result: %f\n", sum);
-  }  // use this scope to ensure the lifetime of "A" ends before finalize
+    Kokkos::View<ele*> prp ("aos_prp", N);
+  
+#if defined(KOKKOS_ENABLE_CXX11_DISPATCH_LAMBDA)
+    
+    Kokkos::parallel_for(
+        N, KOKKOS_LAMBDA(const int i) {
+          pos(i, 0) = i;
+          pos(i, 1) = i + 100.0;
+        });
+
+    struct_of_arrays(prp_s, prp_v, prp_t, pos, N);
+    array_of_structs(prp, pos, N);
+
+
+#else
+    
+    printf("Error: While building Kokkos, the option -DKokkos_ENABLE_CUDA_LAMBDA=true needs to be specified.");
+
+#endif
+  }
   Kokkos::finalize();
 }
