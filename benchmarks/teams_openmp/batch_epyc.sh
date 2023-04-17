96e8bc884c01ca9e3e6054b40d1594ab4f0d4e67
Serhii Yaskovets <yaskovet@mpi-cbg.de>
2023-04-17 17:31:22 +0200

    Change batch script for romeo

--- benchmarks/teams_openmp/batch_epyc.sh
+++ benchmarks/teams_openmp/batch_epyc.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-#SBATCH --partition=romeo 
+#SBATCH --partition=romeo
 #SBATCH --mem=16000M 
-#SBATCH --ntasks=1 
-#SBATCH --cpus-per-task=128
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --sockets-per-node=1 
+#SBATCH --cores-per-socket=64 
+#SBATCH --threads-per-core=1
 #SBATCH --time=00:30:00
-#SBATCH --error=e.out
-#SBATCH --output=output.out
-
+#SBATCH --error=epyc.err
+#SBATCH --output=epyc.out
 
 
 omp_threads=64
@@ -14,11 +17,11 @@ omp_threads=64
 module purge > /dev/null 2>&1
 
 # Load the module environment suitable for the job
-module load OpenMPI/4.0.5-GCC-10.2.0
+module load GCCcore/10.2.0
 
 export OMP_NUM_THREADS=$omp_threads
 export OMP_PROC_BIND=true
-export OMP_PLACES="{0}:128:2"
+export OMP_PLACES="{0}:64:1"
 
 cd /scratch/ws/0/seya960b-paper/romeo/paper_europar-1/benchmarks/teams_openmp
 ./a.out
